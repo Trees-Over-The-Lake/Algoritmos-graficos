88900f074ca6207e778593ee20ccacda84c4b640
Lucas Santiago <lu.santi.oli@gmail.com>
2021-08-12 15:31:29 -0300

    feat: janela abre e vê onde o mouse clicou

--- Trabalho pratico 1/main.go
+++ Trabalho pratico 1/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"runtime"
+	"time"
 
 	"github.com/go-gl/glfw/v3.3/glfw"
 )
@@ -17,7 +19,14 @@ func main() {
 	}
 	defer glfw.Terminate()
 
-	janela, err := glfw.CreateWindow(640, 480, "Trabalho prático 1", nil, nil)
+	// // OR using GLFW:
+	// vk.SetGetInstanceProcAddr(glfw.GetVulkanGetInstanceProcAddress())
+
+	// if err := vk.Init(); err != nil {
+	// 	panic(err)
+	// }
+
+	janela, err := glfw.CreateWindow(1280, 720, "Trabalho prático 1", nil, nil)
 	if err != nil {
 		panic(err)
 	}
@@ -26,6 +35,12 @@ func main() {
 
 	for !janela.ShouldClose() {
 		// Inicializar janela
+		if janela.GetMouseButton(glfw.MouseButton1) == glfw.Press {
+			posX, posY := janela.GetCursorPos()
+			fmt.Printf("Posição X: %v | Posição Y: %v\n", posX, posY)
+			time.Sleep(time.Millisecond * 250)
+		}
+
 		janela.SwapBuffers()
 		glfw.PollEvents()
 
