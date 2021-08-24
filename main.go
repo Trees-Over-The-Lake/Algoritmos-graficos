5708426b46bf858756b3d22095303eb2eb5a219d
Lucas Santiago <lu.santi.oli@gmail.com>
2021-08-24 12:11:38 -0300

    feat: initial setup

--- main.go
+++ main.go
@@ -10,23 +10,18 @@ func main() {
 	}
 	defer sdl.Quit()
 
-	window, err := sdl.CreateWindow("Trabalho prático 1", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+	janela, err := sdl.CreateWindow("Trabalho prático 1", sdl.WINDOWPOS_CENTERED, sdl.WINDOWPOS_CENTERED,
 		800, 600, sdl.WINDOW_SHOWN)
 	if err != nil {
 		panic(err)
 	}
-	defer window.Destroy()
+	defer janela.Destroy()
 
-	surface, err := window.GetSurface()
+	superficie, err := janela.GetSurface()
 	if err != nil {
 		panic(err)
 	}
-	surface.FillRect(nil, 0)
-
-	rect := sdl.Rect{0, 0, 200, 200}
-	//triangle := sdl.Re
-	surface.FillRect(&rect, 0xffff0000)
-	window.UpdateSurface()
+	superficie.FillRect(nil, 0)
 
 	running := true
 	for running {
@@ -37,7 +32,23 @@ func main() {
 				running = false
 			}
 		}
+
+		// Pegar a ultima atualização dos inputs
+		sdl.PumpEvents()
+		// Pegando a posição do mouse na tela
+		mouseX, mouseY, estado := sdl.GetMouseState()
+
+		DesenharPontoBranco(janela, superficie, mouseX, mouseY)
+		if estado&sdl.Button(sdl.BUTTON_LEFT) == 0 {
+			superficie.FillRect(nil, 0)
+		}
 	}
+}
+
+// Retornar um ponto branco
+func DesenharPontoBranco(janela *sdl.Window, superficie *sdl.Surface, X int32, Y int32) {
+	superficie.FillRect(&sdl.Rect{X, Y, 30, 30}, 0xffffffff)
+	janela.UpdateSurface()
 }
 
 // Desenhar linha usando algoritmo de Bresenhan
